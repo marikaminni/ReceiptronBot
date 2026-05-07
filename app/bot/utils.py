f051f7d40e7bbd09c45b2ac49e46b4cb715b149a
Marika <marika.minni@outlook.it>
2026-05-07 11:04:41 +0200

    add function to display query data

--- app/bot/utils.py
+++ app/bot/utils.py
@@ -1,10 +1,18 @@
+"""Return info from parsed data and receipt id"""
 def format_message_receipt(parsed_data: dict, receipt_id: int) -> str:
     return(
         "✅ Scontrino salvato!\n\n"
         "Recap: \n\n"
-        f"- Receipt:ID: {receipt_id}\n"
+        f"- ReceiptID: {receipt_id}\n"
         f"- Date: {parsed_data['date']}\n"
         f"- Negozio: {parsed_data['negozio']}\n"
         f"- Categoria: {parsed_data['category']}\n"
         f"- Totale: € {parsed_data['total']:.2f}\n"
     )
+"""Return filtered data from db based on period of filtering"""
+def format_db_filter(db_data) -> str:
+    recap= "Recap totale per categoria: \n"
+    for data in db_data:
+        recap += f"- {data["category"]}: €{data["Total"]}\n"
+
+    return recap
