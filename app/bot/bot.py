1d7198996b85009fb3bbe902ab3699cee4b7583a
Marika <marika.minni@outlook.it>
2026-05-07 11:03:03 +0200

    add function recap to get info about specific period

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -1,15 +1,19 @@
 from telegram import Update
-from telegram.ext import Application, MessageHandler, ContextTypes, filters, CommandHandler
+from telegram.ext import Application, MessageHandler, filters, CommandHandler
 from config import BOT_TOKEN, IMAGES_DIR
 from app.services.ai_parser import parse_receipt
-from app.db.db import init_db, save_receipt
-from app.bot.utils import format_message_receipt
+from app.db.db import init_db
+from app.db.query import save_receipt, filter_period
+from app.bot.utils import format_message_receipt, format_db_filter
+from app.bot.period_formatter import period_formatter
 import uuid
 import os
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Hi, send me an image of a receipt")
 
-async def handle_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start(update: Update, context) -> None:
+    await update.message.reply_text("Hi, send me an image of a receipt")
+    #todo: aggiungere command help per descrivere funzionalità bot
+
+async def handle_image(update: Update, context) -> None:
     photo= update.message.photo[-1]
 
     #set unique id and image path
@@ -22,7 +26,7 @@ async def handle_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
     #send to ai_parser
     parsed_data= parse_receipt(image_path)
-    #save to db parsered data
+    #save to db parsed data
     receipt_id=save_receipt(parsed_data)
     #delete image
     os.remove(image_path)
@@ -30,13 +34,25 @@ async def handle_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     #bot answer
     await update.message.reply_text(format_message_receipt(parsed_data, receipt_id))
 
+async def recap(update: Update, context ) -> None:
+    #take the argument after the command, join the chunk into string
+    period= "".join(context.args)
+    #obtain number of days to subtract
+    formatted_period= period_formatter(period)
+
+    #filter data based on period
+    filtered_receipt= filter_period(formatted_period)
+
+    #bot answer with formatted data
+    await update.message.reply_text(format_db_filter(filtered_receipt))
+
 def main():
     init_db()
     # Create the Application and pass it your bot's token.
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.PHOTO,handle_image))
-
+    app.add_handler(MessageHandler(filters.PHOTO, handle_image))
+    app.add_handler(CommandHandler("recap", recap))
     app.run_polling()
 if __name__ == "__main__":
     main()
