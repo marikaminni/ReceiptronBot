ffc0390353eff208b9c81b6f0254655788e3a819
Marika <marika.minni@outlook.it>
2026-05-07 11:09:30 +0200

    move query to dedicated file

--- app/db/db.py
+++ app/db/db.py
@@ -15,18 +15,3 @@ def init_db():
             print("Table created successfully!")
     except sqlite3.OperationalError as e:
         print("Failed to open database:", e)
-
-def save_receipt(data: dict) -> int:
-    try:
-        with sqlite3.connect(DB_PATH) as conn:
-            cur= conn.cursor()
-            cur.execute("""
-            INSERT INTO receipt (date, negozio, category, total)
-            VALUES (:date, :negozio, :category, :total)""", data)
-
-            print("Insertion was successful!")
-
-            #get the id of the last inserted row
-            return cur.lastrowid
-    except sqlite3.OperationalError as e:
-        print("Failed to open database:", e)
